35304e35b8080fc890e9004b829bee15f12ca80f
Bruno Martins <bgcngm@gmail.com>
2019-05-30 13:00:04 +0000

    msm8917-common: Force HAL1 for some package list

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -65,6 +65,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.gyro.android=1 \
     persist.camera.HAL3.enabled=1 \
     persist.camera.is_type=1 \
+    vendor.camera.hal1.packagelist=com.whatsapp,com.intsig.camscanner,com.instagram.android,com.snapchat.android \
     vidc.enc.dcvs.extra-buff-count=2
 
 # CNE
