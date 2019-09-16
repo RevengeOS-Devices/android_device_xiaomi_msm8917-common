aee178fe187bb2582cc5600d9bb52a806d21f1d3
Nick <nikhilnick00007@gmail.com>
2019-09-16 11:27:47 +0000

    msm8917-common: Add health hal
    Signed-off-by: Nick <nikhilnick00007@gmail.com>

--- msm8917.mk
+++ msm8917.mk
@@ -181,8 +181,8 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.0-service.rolex \
+    charger_res_images
 
 # HIDL
 PRODUCT_PACKAGES += \
